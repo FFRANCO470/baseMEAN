ccad08aa96fda3451f34e7ee4c4100bbe7d715dc
FFRANCO470 <fjfranco697@misena.edu.co>
2021-03-18 07:33:22 -0500

    proyecto base mean

---
+++ database/config.js
@@ -0,0 +1,20 @@
+//solicitar la dependecia de mongoose para operar en la base de datos desde el coidgo
+import mongoose from 'mongoose';
+//crear conexion con la base de datos
+const dbConnection = async () =>{
+    //si se establece la conexion con la base de datos
+    try {
+        await mongoose.connect(process.env.MONGODB_CNX, {
+            useNewUrlParser: true,
+            useUnifiedTopology:true,
+            useCreateIndex : true,
+            useFindAndModify:false
+        });
+        console.log('conectado a la base de datos');
+    }
+    //si no se establece la conexion con la base de datos
+     catch (error) {
+        throw new Error('error al conectarse con la base de datos')
+    }
+}
+export default dbConnection
---
+++ models/server.js
@@ -0,0 +1,42 @@
+//solicitar recursos de las dependencias de express y cors
+import express from 'express';
+import cors from 'cors';
+//solicitar informacion de la conexion de base de datos
+import dbConnection from '../database/config.js';
+//crear la clase de servidor
+class Server{
+    constructor(){
+        //puerto de conexion
+        this.port = process.env.PORT;
+        //servidor
+        this.app = express();
+        //conectar con base de datos
+        this.app = conectarDB();
+        //middlewares
+        this.app = middlewares();
+        //rutas
+        this.app = routes();
+    }
+    //metodo para conetar con base de datos
+    async conectarDB(){
+        await dbConnection();
+    }
+    //metodos para llamar los middlewares
+    middlewares(){
+        this.app.use(express());
+        this.app.use(cors());
+        this.app.use(express.static('public'));
+    }
+    //metodos para las rutas 
+    routes(){
+
+    }
+    //metodo para iniciar el servidor
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log(`servidor corriendo en el puerto ${this.port}`);
+        });
+    }
+}
+//exportar la clase servidor
+export default Server
